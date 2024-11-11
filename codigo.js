2bc1882c1942dfeabd7ca809e651f9459cdd32d2
GuilhermeLopes06 <guilhermedemoraes54@gmail.com>
2024-11-11 18:04:25 -0300

    Filtros

--- codigo.js
+++ codigo.js
@@ -1,4 +1,4 @@
-const url = "https://botafogo-atletas.mange.li/2024-1/";
+const urlBase = "https://botafogo-atletas.mange.li/2024-1/";
 
 const pega_json = async (caminho) => {
     const resposta = await fetch(caminho);
@@ -12,7 +12,6 @@ const manipulaClick = (e) => {
     const id = e.currentTarget.dataset.id;
     const url = `detalhes.html?id=${id}`;
 
-    // Armazenar em cookie, localStorage e sessionStorage
     document.cookie = `id=${id}`;
     localStorage.setItem('id', id);
     localStorage.setItem('dados', JSON.stringify(e.currentTarget.dataset));
@@ -45,31 +44,34 @@ const montaCard = (atleta) => {
     return cartao;
 };
 
-// Carregar os dados dos atletas e exibir os cards
-pega_json(`${url}masculino`).then((r) => {
-    r.forEach((ele) => container.appendChild(montaCard(ele)));
-});
+// Função para carregar dados com base no filtro selecionado
+const carregarDados = (filtro) => {
+    container.innerHTML = ""; // Limpa os cards existentes
+    pega_json(`${urlBase}${filtro}`).then((r) => {
+        r.forEach((ele) => container.appendChild(montaCard(ele)));
+    });
+};
 
-// Função para verificar login e redirecionar se não estiver logado
+// Carrega o filtro 'masculino' por padrão ao abrir a página
+carregarDados('masculino');
+
+// Verificar login e redirecionar se não estiver logado
 if (!sessionStorage.getItem('logado') && window.location.pathname.includes("home.html")) {
-    window.location.href = "index.html"; // Redireciona para a página de login se não estiver logado
+    window.location.href = "index.html";
 }
 
 const manipulaBotao = () => {
     const texto = document.getElementById('senha').value;
     if (hex_md5(texto) === '5029cc9dd0295ded2f500084635c18c1') {
         sessionStorage.setItem('logado', 'sim');
-        window.location.href = "home.html"; // Redireciona para a página home após login bem-sucedido
+        window.location.href = "home.html";
     } else {
         alert('Você errou a senha');
     }
 };
 
-
-
 // Função de logout para limpar o sessionStorage
 document.getElementById('logout').onclick = () => {
-    sessionStorage.removeItem('logado');  // Remove a flag de login
-    window.location.href = "index.html";   // Redireciona para a página de login
+    sessionStorage.removeItem('logado');
+    window.location.href = "index.html";
 };
-
